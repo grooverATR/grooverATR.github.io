b3cd0c8a7e26920a99d16ac3c113e8ad3f276802
grooverATR <120337271+grooverATR@users.noreply.github.com>
2023-03-12 19:15:15 +0200

    עוד ניסיון להעלות תמונה

--- src/main.js
+++ src/main.js
@@ -49,7 +49,6 @@ let track_list = [
 {name:"אלף ואחת אמריקות",artist:"וזהו",image:"src/graphics/20.png",path:"https://od.lk/s/NzZfOTIwNzY3MDFf/20%20%D7%90%D7%9C%D7%A3%20%D7%95%D7%90%D7%97%D7%AA%20%D7%90%D7%9E%D7%A8%D7%99%D7%A7%D7%95%D7%AA.mp3"},
 {name:"המומינים",artist:"המופלאים",image:"src/graphics/21.png",path:"https://od.lk/s/NzZfOTIwNzY3MDNf/21%20%D7%94%D7%9E%D7%95%D7%9E%D7%99%D7%A0%D7%99%D7%9D.mp3"},
 {name:"הדוב",artist:"בבית הכחול",image:"src/graphics/22.png",path:"https://od.lk/s/NzZfOTIwNzY2MDdf/22%20%D7%94%D7%93%D7%95%D7%91%20%D7%91%D7%91%D7%99%D7%AA%20%D7%94%D7%9B%D7%97%D7%95%D7%9C%20%28%D7%A9%D7%9C%D7%95%D7%9D%20%D7%A9%D7%9C%D7%95%D7%9D%20%D7%9C%D7%94%D7%AA%D7%A8%D7%90%D7%95%D7%AA%29.mp3"},
-
 ];
 
 function loadTrack(track_index) {
